5a79e55f3146d7e7503be8cb20d77dbb75e6edc0
BReily89 <BrandonReily.br@gmail.com>
2017-11-28 15:41:43 -0500

    projects and homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Projects from './/projects/Projects';
 
 class HomePage extends Component {
     render() {
---
+++ src/components/projects/Projects.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+    render() {
+        return (
+            <div>
+            <div>
+                <h2>Jeopardy</h2>
+            </div>
+            <div>
+                <h2>Neighborhood restaurant List</h2>
+            </div>
+            <div>
+                <h2>Lazy Lifter</h2>
+            </div>
+            <div>
+                <h2>Self Centered Selfie</h2>
+            </div>
+            </div>
+        );
+    }
+}
+
+export default Projects;
