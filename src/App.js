8eb03189dbf8cb63239f940353221a8ff3c3d7a4
BReily89 <BrandonReily.br@gmail.com>
2017-11-28 15:28:07 -0500

    initial setup complete

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom";
+import HomePage from './components/HomePage';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Route exact path="/"component={HomePage} />
+      </Router>
+    );
+  }
+}
+
+export default App;
