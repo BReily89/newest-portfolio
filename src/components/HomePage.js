476a4e544bcea317c7206c67c61bca43d636b873
BReily89 <BrandonReily.br@gmail.com>
2017-11-29 15:13:15 -0500

    pushing for job

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,7 +6,11 @@ class HomePage extends Component {
         return (
             <div>
             <div>
-                <h1>this is the homepage</h1>
+                <h1>The Portfolio of Brandon Reily </h1>
+                <h2><img  src="https://i.imgur.com/faDPfH1.jpg?1"/></h2>
+                
+
+                
                 <Projects />
             </div>
             </div>
